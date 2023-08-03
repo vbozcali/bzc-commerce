168accc6145d1a1356833fc95d2f2734d9ec1a96
Veysel Bozçalı <vbozcali049@gmail.com>
2023-08-03 14:55:02 +0300

    Directory Component Created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -30,13 +30,7 @@ const App = () => {
   ]
 
   return (
-    <div className="categories-container">
-      {
-        categories.map((category) => (
-          <CategoryItem key={category.id} category={category} />        
-        ))
-      }
-    </div>
+    <Directory categories={categories} />
   );
 }
 
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,11 +1,12 @@
 import './category-item.styles.scss';
 
 const CategoryItem = ({category}) => {
-    const { id, title, imageUrl } = category;
+    const { title, imageUrl } = category;
 
     return (
         <div className="category-container">
             <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+            
             <div className="category-body-container">
                 <h2>{title}</h2>
                 <p>Shop Now</p>
