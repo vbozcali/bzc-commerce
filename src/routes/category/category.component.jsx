2ef9ebd3a1d7423c9f3f619e417c434eb6f38d26
Veysel Bozçalı <43677792+vbozcali@users.noreply.github.com>
2023-08-27 19:46:40 +0300

    File Renaming and Styling Updates

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import { CategoriesContext } from "../../context/category.context";
 import ProductCard from "../../components/product-card/product-card.component";
@@ -14,12 +14,15 @@ const Category = () => {
     }, [category, categoriesMap]);
 
     return (
-        <div className="category-container">
-            {products
-                && products.map((product) => (
-                    <ProductCard product={product} key={product.id} />
-                ))}
-        </div>
+        <Fragment>
+            <h1 className="title">{category.toUpperCase()}</h1>
+            <div className="category-container">
+                {products
+                    && products.map((product) => (
+                        <ProductCard product={product} key={product.id} />
+                    ))}
+            </div>
+        </Fragment>
     )
 }
 
