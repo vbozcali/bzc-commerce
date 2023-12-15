faf23cd4d562d0a253185293c1fb4480587279c6
Veysel Bozçalı <43677792+vbozcali@users.noreply.github.com>
2023-12-15 17:20:01 +0300

    Using createSlice

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,33 +1,21 @@
-import { USER_ACTION_TYPES } from "./user.types";
+// import { USER_ACTION_TYPES } from "./user.types";
+import { createSlice } from "@reduxjs/toolkit";
 
 const INITIAL_STATE = {
-    currentUser: null,
-    isLoading: false,
-    error: null
+    currentUser: null
 }
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-    const { type, payload } = action;
-
-    switch (type) {
-        case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-            return {
-                ...state,
-                currentUser: payload
-            }
-        case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
-            return {
-                ...state,
-                currentUser: null
-            }
-        case USER_ACTION_TYPES.SIGN_IN_FAILED:
-        case USER_ACTION_TYPES.SIGN_OUT_FAILED:
-        case USER_ACTION_TYPES.SIGN_UP_FAILED:
-            return {
-                ...state,
-                error: payload
-            }
-        default:
-            return state;
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: INITIAL_STATE,
+    reducers: {
+        setCurrentUser(state, action) {
+            state.currentUser = action.payload
+        }
     }
-}
+});
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
+
