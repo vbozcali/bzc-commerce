5dbbb029a00a72945075c1ba7392dc11d69781e0
Veysel Bozçalı <43677792+vbozcali@users.noreply.github.com>
2024-08-14 19:23:01 +0300

    Update app-logo.component.jsx

--- src/components/app-logo/app-logo.component.jsx
+++ src/components/app-logo/app-logo.component.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { LogoContainer } from "./app-logo.styles";
+import logo from '../assets/logo.svg'; 
 
 export const AppLogo = () => {
     return (
         <LogoContainer>
             <Link to="/">
-                <img style={{ width: 300 }} src="https://i.ibb.co/WsdZL4t/bzc-logo.png" alt="" />
+                <img style={{ width: 300 }} src="{logo}" alt="" />
             </Link>
         </LogoContainer>
     )
